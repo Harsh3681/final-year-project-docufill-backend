4793d11d6c85326e8ca9748a42c825f577d5260f
Ajay Rathod <rathodajay1202@gmail.com>
2024-03-15 08:03:55 +0530

    host and port

--- index.py
+++ index.py
@@ -72,5 +72,5 @@ def home():
 def stream_words():
     return Response(generate_words(), mimetype='text/plain')
 
-# if __name__ == '__main__':
-#     socketio.run(app,allow_unsafe_werkzeug=True)
+if __name__ == '__main__':
+    socketio.run(app,host='0.0.0.0',port=8000,allow_unsafe_werkzeug=True)
